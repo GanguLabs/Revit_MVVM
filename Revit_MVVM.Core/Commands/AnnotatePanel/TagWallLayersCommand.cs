a645ec2dcf253762bccfde9d990ec662c424954a
Gangula2 <gangulaanwesh@gmail.com>
2022-10-30 11:17:36 +0530

    Chapter 3 - Tag Wall Layers Command & Create Text Note Element

--- Revit_MVVM.Core/Commands/AnnotatePanel/TagWallLayersCommand.cs
+++ Revit_MVVM.Core/Commands/AnnotatePanel/TagWallLayersCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,14 +15,82 @@ namespace Revit_MVVM.Core
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            TaskDialog msgWindow = new TaskDialog("info")
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            // check if the current Revit File is a Project, not a Revit Family
+            if (doc.IsFamilyDocument)
             {
-                MainContent = "Test Message window from Revit",
-                MainIcon = TaskDialogIcon.TaskDialogIconInformation,
-                CommonButtons = TaskDialogCommonButtons.Ok
+                Message.Display("Cant use command in family document", WindowType.Warning);
+                return Result.Cancelled;
+            }
+
+            View activeView = uiDoc.ActiveView;
+
+            bool canCreateTextNoteInView = false;
+
+            switch (activeView.ViewType)
+            {
+                case ViewType.FloorPlan:
+                    canCreateTextNoteInView = true;
+                    break;
+                case ViewType.CeilingPlan:
+                    canCreateTextNoteInView = true;
+                    break;
+                case ViewType.Detail:
+                    canCreateTextNoteInView = true;
+                    break;
+                case ViewType.Elevation:
+                    Message.Display("This is an Elevation View.\nPlease make sure to set a Workplane to any of the wall faces", WindowType.Information);
+                    canCreateTextNoteInView = true;
+                    break;
+            }
+
+            if (!canCreateTextNoteInView)
+            {
+                Message.Display("Text Note element can't be created in current view", WindowType.Error);
+                return Result.Cancelled;
+            }
+
+            var selFilter = new SelectionFilterCategory("Walls");
+            var selectionReference = uiDoc.Selection.PickObject(ObjectType.Element, selFilter, "Select one basic wall.");
+            Element selectionElement = doc.GetElement(selectionReference);
+
+            Wall wall = selectionElement as Wall;
+
+            if (wall.IsStackedWall)
+            {
+                Message.Display("Wall you selectced is of category Stacked Wall.\nIt's not supported byy this command.", WindowType.Warning);
+                return Result.Cancelled;
+            }
+
+            var pt = uiDoc.Selection.PickPoint("Pick location to place Text Note.");
+            var layers = wall.WallType.GetCompoundStructure().GetLayers();
+
+            var msg  = new StringBuilder();
+
+            foreach (var layer in layers)
+            {
+                var material = doc.GetElement(layer.MaterialId) as Material;
+
+                msg.AppendLine(layer.Function.ToString() + " " + material.Name + " " + layer.Width.ToString());
+            }
+
+            var textNoteOptions = new TextNoteOptions
+            {
+                VerticalAlignment = VerticalTextAlignment.Top,
+                HorizontalAlignment = HorizontalTextAlignment.Left,
+                TypeId =doc.GetDefaultElementTypeId(ElementTypeGroup.TextNoteType),
             };
 
-            msgWindow.Show();
+            using (var transaction = new Transaction(doc))
+            {
+                transaction.Start("Tag Wall Layers Command");
+
+                var textNote = TextNote.Create(doc, activeView.Id, pt, msg.ToString(), textNoteOptions);
+
+                transaction.Commit();
+            }
 
             return Result.Succeeded;
         }
---
+++ Revit_MVVM.Core/Commands/Helpers/SelectionFilterCategory.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_MVVM.Core
+{
+    public class SelectionFilterCategory : ISelectionFilter
+    {
+        private string mCategory = "";
+
+        public SelectionFilterCategory(string category)
+        {
+            mCategory = category;
+        }
+        public bool AllowElement(Element elem)
+        {
+            if(elem.Category != null && elem.Category.Name == mCategory)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
